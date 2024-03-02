c3581a9a772467e5c74677c1e4608e5cb75f224c
Paulo-xavier-a <pr3546287@gmail.com>
2024-03-02 09:35:05 -0300

    02

--- js/script.js
+++ js/script.js
@@ -1,24 +1,23 @@
+import { Modal } from "./modal.js"
+import {AlertError} from"./alert.js"
 
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-const Modal = {
-    wrapper: document.querySelector('.modal-wrapper'),
-    message: document.querySelector('.modal .title span'),
-    buttonClose: document.querySelector('.modal button.close'),
 
-
-    open() {    Modal.wrapper.classList.add('open')
-},
-    close() {    Modal.wrapper.classList.remove('open')
-} 
-}
 
 form.onsubmit = event => {event.preventDefault()
 
     const weight = inputWeight.value
     const height = inputHeight.value
+
+const showAlertError = notANumber(weight) || notANumber(height)
+
+if(showAlertError){
+    AlertError.open()
+    return;
+}
 
     const result = IMC(weight, height)
     const message = `Seu IMC é de ${result}`
@@ -27,8 +26,8 @@ form.onsubmit = event => {event.preventDefault()
     Modal.open()
 }
 
-Modal.buttonClose.onclick = () => {
-    Modal.close()
+function notANumber(value){
+    return isNaN(value) || value == ""
 }
 
 function IMC(weight, height) {
